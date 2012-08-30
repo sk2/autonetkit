3b1580d751e932afe5ae79afe4bf9b386aa14d56
Simon Knight <simon.knight@gmail.com>
2012-08-30 20:14:02 +0930

    support host keys

--- autonetkit/deploy.py
+++ autonetkit/deploy.py
@@ -1,29 +1,37 @@
+import autonetkit.log as log
 
 def package(src_dir, target):
-        import tarfile
-        import os
-        tar_filename = "%s.tar.gz" % target
+    log.info("Packaging %s" % src_dir)
+    import tarfile
+    import os
+    tar_filename = "%s.tar.gz" % target
 #time.strftime("%Y%m%d_%H%M", time.localtime())
-        tar = tarfile.open(os.path.join(tar_filename), "w:gz")
-        tar.add(src_dir)
-        tar.close()
-        return tar_filename
+    tar = tarfile.open(os.path.join(tar_filename), "w:gz")
+    tar.add(src_dir)
+    tar.close()
+    print tar_filename
+    return tar_filename
 
 
-def transfer(host, username, local, remote):
+def transfer(host, username, local, remote, key_filename = None):
+    log.info("Transferring lab to %s" % host)
     import paramiko
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(
         paramiko.AutoAddPolicy())
-    ssh.connect(host, username = username)
+    if key_filename:
+        ssh.connect(host, username = username, key_filename = key_filename)
+    else:
+        ssh.connect(host, username = username)
     ftp = ssh.open_sftp()
     ftp.put(local, remote)
     ftp.close()
 
-def extract(host, tar_file, cd_dir):
+def extract(host, username, tar_file, cd_dir, key_filename = None):
     from Exscript import Account
     from Exscript.util.start import start
     from Exscript.util.match import first_match
+    from Exscript import PrivateKey
     from Exscript.protocols.Exception import InvalidCommandException
 
     def starting_host(protocol, index, data):
@@ -53,12 +61,17 @@ def extract(host, tar_file, cd_dir):
                 print "Starting lab"
                 conn.execute(start_command)
         first_match(conn, r'^The lab has been started')
-        print "HERE"
         conn.send("exit")
 
 #TODO: need to capture and handle tap startup
 
-    accounts = [Account("sknight")] 
+    if key_filename:
+        key = PrivateKey.from_file(key_filename)
+        accounts = [Account(username, key = key)] 
+    else:
+        accounts = [Account(username)] 
+
+
     hosts = ['ssh://%s' % host]
     start(accounts, hosts, do_something, verbose = 2)
 
