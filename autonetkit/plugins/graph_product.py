53b708ad7e74375b6847528954dfc74460cbcbfb
Simon Knight <simon.knight@gmail.com>
2012-09-10 15:57:22 +0930

    more graph products

--- autonetkit/plugins/graph_product.py
+++ autonetkit/plugins/graph_product.py
@@ -3,13 +3,13 @@ import autonetkit.ank_utils as ank_utils
 import autonetkit.ank as ank
 import os
 import pprint
+import autonetkit.log as log
 
 def expand(G_in):
     """ Expands out graph products. G is the source "backbone" graph. H_x is the "PoP template" graphs
     """
     graph_unwrapped = ank_utils.unwrap_graph(G_in)
     G = graph_unwrapped.copy()
-    print G.edges(data=True)
     
     ank.set_node_default(G_in, G_in, pop_template = "default")
     
@@ -21,42 +21,25 @@ def expand(G_in):
         pop_graph = nx.read_graphml(template_filename).to_undirected()
         nx.relabel_nodes(pop_graph, dict((n, data.get('label')) for n, data in pop_graph.nodes(data=True)), copy = False)
         templates[template] = pop_graph
-    print templates
-    for name, graph in templates.items():
-        print graph.nodes(data=True)
-
-# Set PoP based on label
-    for node in G_in:
-        node.pop = node.label
-        print node.pop
 
     # construct new graph
     G_out = nx.Graph() #TODO: what about bidirectional graphs?
-    G_out.add_nodes_from(node_list(G, templates))
-    print G_out.nodes()
-# drop in replacement graph
-    #remove_nodes = G_out.nodes()[3:]
-    #print "remove_nodes", remove_nodes
-    #G_out.remove_nodes_from(remove_nodes)
-    print G_out.nodes()
+    G_out.add_nodes_from(expand_nodes(G, templates))
+    print intra_pop_links(G, templates)
+    G_out.add_edges_from(intra_pop_links(G, templates))
+    G_out.add_edges_from(inter_pop_links(G, templates))
+    print G_out.edges()
+
+    for s, t in G_out.edges():
+        G_out[s][t]['type'] = 'physical' # ensure copied across
+    
     import itertools
     gen = itertools.count(10, 80)
-    gen2 = itertools.count(10, 80)
-    for node in G_out:
-        G_out.node[node].update( {
-            'x': gen.next(),
-            'y': gen2.next() + 50,
-            'asn': 1,
-            'label': "%s_%s" % (node[1], node[0]),
-            'device_type': "router",
-            })
-
     
     # Update properties based on co-ordinates
     for node in G_out:
         u, v = node
         template = G.node[u]['pop_template']
-        print u, v
         u_properties = G.node[u].copy()
         v_properties = dict(templates[template].node[v]) # create copy to append with
         x = float(u_properties.get('x')) + float(v_properties.get('x'))
@@ -66,41 +49,80 @@ def expand(G_in):
         u_properties['y'] = y
         u_properties['label'] = "%s_%s" % (v, u)
         u_properties['id'] = "%s_%s" % (v, u)
-        #del properties['pop_template']
-        print u_properties
+        u_properties['pop'] = u
+        del u_properties['pop_template']
         G_out.node[node] = u_properties
 
-    edges = [(s, t, {'edge_id': gen.next(), 'type': 'physical'}) for s in G_out for t in G_out if s != t]
-    edges = edges[:3]
-    G_out.add_edges_from(edges)
     nx.relabel_nodes(G_out, dict( ((u, v), "%s_%s" % (v, u)) for (u, v) in G_out), copy = False)
-    print G_out.edges(data=True)
 
     G_in._replace_graph(G_out)
-    print list(G_in.edges())
 
     return
-
-
-
-    nx.relabel_nodes(G_out, dict( ((u, v), "%s_%s" % (v, u)) for (u, v) in G_out), copy = False)
-    pprint.pprint( graph_unwrapped.nodes(data=True))
-    print "out"
-    pprint.pprint( G_out.nodes(data=True))
-    G_out.add_edge("r1_Melbourne", "r1_Sydney", edge_id = "a_b")
-    print G_out.edges()
-    graph_unwrapped.clear()
-    G_in._replace_graph(G_out)
-    print list(G_in.nodes())
-    print list(G_in.edges())
 
 #TODO: use "interpop" instead of "rooted"
 
 
-def node_list(G, templates):
+def expand_nodes(G, templates):
     # TODO: work out how to retain node attributes
     return [ (u,v) for u in G for v in templates[G.node[u]['pop_template']] ]
 
+def intra_pop_links(G, templates):
+    return [ ((u,v1), (u,v2)) for u in G for (v1, v2) in templates[G.node[u]['pop_template']].edges() ]
+
+
+def inter_pop_links(G, templates, default_operator='cartesian'):
+    #TODO:: list any edges without operator marked on them
+# for brevity, Hx refers to templatex
+    edges = []
+    cartesian_operators = set(["cartesian", "strong"])
+    tensor_operators = set(["tensor", "strong"])
+    for (u1, u2) in G.edges():
+        try:
+            operator = G[u1][u2]['operator']
+        except KeyError:
+            operator =  default_operator
+
+        if operator == "None": # from Graphml
+            operator =  default_operator
+
+        H1 = templates[G.node[u1]['pop_template']]
+        H2 = templates[G.node[u2]['pop_template']]
+# Node lists - if 'root' set then only use root nodes
+        N1 = [n for n, d in H1.nodes(data=True) if 'interpop' in d and d['interpop']]
+        if not len(N1):
+            N1 = [n for n in H1] # no nodes marked interpop
+        N2 = [n for n, d in H1.nodes(data=True) if 'interpop' in d and d['interpop']]
+        if not len(N2):
+            N2 = [n for n in H2] # no nodes marked interpop
+        
+        log.debug("Adding edges for (%s,%s) with operator %s" % (u1, u2, operator))
+
+        log.debug("H nodes for u1 %s: %s" % ( G.node[u1]['pop_template'], ", ".join(str(N1))))
+        log.debug("H nodes for u2 %s: %s" % ( G.node[u2]['pop_template'], ", ".join(str(N2))))
+# 'root' not set
+#TODO: fold rooted back into special case of cartesian - just do the same for now
+        if operator == 'rooted':
+            product_edges = [((u1, v1), (u2, v2)) for v1 in N1 for v2 in N2
+                    if H1.node[v1].get("interpop") == H2.node[v2].get("interpop") == True ]
+            log.debug("Rooted product edges for (%s,%s): %s" % (u1, u2, product_edges))
+            edges += product_edges
+
+        if operator == 'lexical':
+            product_edges = [((u1, v1), (u2, v2)) for v1 in N1 for v2 in N2]
+            log.debug("Lexical product edges for (%s,%s): %s" % (u1, u2, product_edges))
+            edges += product_edges
+
+        if operator in cartesian_operators:
+            product_edges = [((u1, v1), (u2, v2)) for v1 in N1 for v2 in N2 if v1 == v2]
+            log.debug("Cartesian product edges for (%s,%s): %s" % (u1, u2, product_edges))
+            edges += product_edges
+        if operator in tensor_operators:
+            product_edges = [((u1, v1), (u2, v2)) for v1 in N1 for v2 in N2
+                    if  H1.has_edge(v1, v2) or H2.has_edge(v1,v2)]
+            log.debug("Tensor product edges for (%s,%s): %s" % (u1, u2, product_edges))
+            edges += product_edges
+
+    return edges
 
 
 #TODO: What about edge ids?
