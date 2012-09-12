f491fa1fc83cb157d77d88473418d24d3d2d066d
Simon Knight <simon.knight@gmail.com>
2012-09-12 12:05:42 +0930

    removing outdated webserver

--- autonetkit/change_monitor.py
+++ autonetkit/change_monitor.py
@@ -1,9 +1,8 @@
 import networkx as nx
 import os
+import autonetkit.log as log
 
 def check_for_change(input_file, anm):
-    print "input_file", input_file, "anm", anm
     timestamp_file = os.stat(input_file).st_mtime
-    print anm['input']._graph.graph
     timestamp_graph = anm.timestamp
     return timestamp_file != timestamp_graph
