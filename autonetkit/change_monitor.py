89bdb1f2a9fed3ea8a166bd6063b987ba3515ed0
Simon Knight <simon.knight@gmail.com>
2012-09-12 13:37:34 +0930

    debugging

--- autonetkit/change_monitor.py
+++ autonetkit/change_monitor.py
@@ -5,4 +5,5 @@ import autonetkit.log as log
 def check_for_change(input_file, anm):
     timestamp_file = os.stat(input_file).st_mtime
     timestamp_graph = anm.timestamp
+    print timestamp_graph, timestamp_file
     return timestamp_file != timestamp_graph
