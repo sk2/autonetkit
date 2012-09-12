aea8f60f1a5710731ec1c787f6eab129dd0dc422
Simon Knight <simon.knight@gmail.com>
2012-09-12 12:02:21 +0930

    fix bug in monitoring

--- autonetkit/change_monitor.py
+++ autonetkit/change_monitor.py
@@ -2,8 +2,8 @@ import networkx as nx
 import os
 
 def check_for_change(input_file, anm):
-     timestamp_file = os.stat(input_file).st_mtime
-     timestamp_graph = anm.overlay.input._graph.graph['timestamp']
-     return timestamp_file != timestamp_graph
-
-
+    print "input_file", input_file, "anm", anm
+    timestamp_file = os.stat(input_file).st_mtime
+    print anm['input']._graph.graph
+    timestamp_graph = anm.timestamp
+    return timestamp_file != timestamp_graph
