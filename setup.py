4fea68ff310db38a21d8338ab1a1d3542dc686f4
Simon Knight <simon.knight@gmail.com>
2012-10-05 16:47:00 +0930

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup (
      name = "autonetkit-v3-dev",
-     version = "0.0.15",
+     version = "0.0.17",
      description = 'Automated configuration generator',
      long_description = 'Automated configuration generator',
 
