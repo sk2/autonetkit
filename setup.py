a8fb22d5af7035ca2b98bb66065bfc0784c8e50a
Simon Knight <simon.knight@gmail.com>
2012-10-08 16:46:15 +1030

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup (
      name = "autonetkit-v3-dev",
-     version = "0.0.18",
+     version = "0.0.20",
      description = 'Automated configuration generator',
      long_description = 'Automated configuration generator',
 
@@ -13,6 +13,7 @@ setup (
      entry_points = {
          'console_scripts': [
              'autonetkit = autonetkit.console_script:main',
+             'ank_webserver = autonetkit.webserver:main',
          ],
      },
 
