9177eb40ef94f5c54bec0f172f1549bb7937e4e7
sk2 <simon.knight@gmail.com>
2012-12-23 17:17:15 +1030

    error handling if no input file

--- autonetkit/console_script.py
+++ autonetkit/console_script.py
@@ -156,11 +156,14 @@ def main():
         with open(options.file, "r") as fh:
             input_string = fh.read()
         timestamp =  os.stat(options.file).st_mtime
-    if options.stdin:
+    elif options.stdin:
         import sys
         input_string = sys.stdin
         now = datetime.now()
         timestamp = now.strftime("%Y%m%d_%H%M%S_%f")
+    else:
+        log.info("No input file specified. Exiting")
+        raise SystemExit
 
     manage_network(input_string, timestamp, build_options = build_options)
 
