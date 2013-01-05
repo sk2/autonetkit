8322286653ec6d2a757aae22c6a453bd8c5db993
sk2 <simon.knight@gmail.com>
2013-01-05 21:38:54 +1030

    more example

--- autonetkit/build_network_simple.py
+++ autonetkit/build_network_simple.py
@@ -2,6 +2,8 @@ import autonetkit
 import autonetkit.anm
 import autonetkit.config
 import autonetkit.load.graphml as graphml
+import autonetkit.ank as ank_utils
+import networkx as nx
 
 def build(input_data, timestamp):
     anm = autonetkit.anm.AbstractNetworkModel()
@@ -26,13 +28,27 @@ def build(input_data, timestamp):
     G_ibgp.add_edges_from(((s, t) for s in routers for t in routers if s.asn == t.asn), bidirectional = True)
 
 
+
+
     # hierarchical
-    G_ibgp = anm.add_overlay("ibgp_rr", G_in.nodes("is_router"), retain = "route_reflector", directed = True)
+    G_ibgp = anm.add_overlay("ibgp_rr", G_in.nodes("is_router"), directed = True)
+
+    graph_phy = ank_utils.unwrap_graph(G_phy)
+    centrality = nx.degree_centrality(graph_phy) 
+    rrs = [n for n in centrality if centrality[n] > 0.13]
+
+    for rr in rrs:
+        G_ibgp.node(rr).route_reflector = True
+
+
     rrs = set(r for r in G_ibgp if r.route_reflector)
     clients = set(G_ibgp) - rrs
     G_ibgp.add_edges_from(((s, t) for s in clients for t in rrs), direction = "up")
     G_ibgp.add_edges_from(((s, t) for s in rrs for t in clients), direction = "down")
     G_ibgp.add_edges_from(((s, t) for s in rrs for t in rrs), direction = "over")
+
+
+
 
     
     return anm
