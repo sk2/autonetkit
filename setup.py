692ff000fd84e6f1434fddb8e3ec957f42acb764
sk2 <simon.knight@gmail.com>
2013-01-18 12:11:35 +1030

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup (
      name = "autonetkit",
-     version = "0.2.12",
+     version = "0.2.17",
      description = 'Automatic configuration generation for emulated networks',
      long_description = 'Automatic configuration generation for emulated networks',
 
