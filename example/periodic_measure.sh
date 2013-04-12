75df0cedc4d52b963a007360c13e0c9410207cdf
sk2 <simon.knight@gmail.com>
2013-04-12 12:39:23 +0930

    modify example input

--- example/periodic_measure.sh
+++ example/periodic_measure.sh
@@ -1,5 +1,5 @@
 while [ 1 ];
 do 
-    python -m autonetkit.console_script -f example.graphml --measure 
+    python -m autonetkit.console_script -f example/singleas_modified.graphml --measure 
         sleep 5;
 done
