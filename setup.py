a9599123b852426c0a8d7fe21f31172ae7675b01
sk2 <simon.knight@gmail.com>
2013-05-13 21:10:28 +0930

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup (
      name = "autonetkit",
-     version = "0.4.2",
+     version = "0.4.3",
      description = 'Automatic configuration generation for emulated networks',
      long_description = 'Automatic configuration generation for emulated networks',
 
