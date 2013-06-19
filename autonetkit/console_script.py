aaf6f3a2a7e673bc023bc8bd4c5594cba389d8f7
sk2 <simon.knight@gmail.com>
2013-06-19 14:36:38 +0930

    tidy

--- autonetkit/console_script.py
+++ autonetkit/console_script.py
@@ -102,9 +102,9 @@ def manage_network(input_graph_string, timestamp, build_options, reload_build=Fa
     command = "traceroute"
 
     log.info("Running traceroute")
-    http_url_measure = ank_messaging.format_http_url("ank-dev", 8001)
-    #http_url_measure = ank_messaging.format_http_url("localhost", 8001)
-    measure_data = ank_messaging.measure(anm, nidb, hosts, command, http_url = http_url_measure)
+    #http_url_measure = ank_messaging.format_http_url("ank-dev", 8000)
+    #update_http(anm, nidb, http_url_measure)
+    measure_data = ank_messaging.measure(anm, nidb, hosts, command, )
     log.info("Finished")
 
 def parse_options(argument_string = None):
