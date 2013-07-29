6a527a0f1074aa0b026758f97e6bb20fdb25a46e
sk2 <simon.knight@gmail.com>
2013-07-29 20:19:45 +0930

    remove unused code

--- autonetkit/change_monitor.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import os
-import autonetkit.log as log
-
-def check_for_change(input_file, anm):
-    timestamp_file = os.stat(input_file).st_mtime
-    timestamp_graph = anm.timestamp
-    print timestamp_graph, timestamp_file
-    return timestamp_file != timestamp_graph
