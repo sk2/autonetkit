09b72888ff889e8b57e44834e91c0e672aaf80aa
sk2 <simon.knight@gmail.com>
2013-07-29 20:18:52 +0930

    add bgp pol tests

--- autonetkit/bgp_pol.py
+++ autonetkit/bgp_pol.py
@@ -244,27 +244,16 @@ class BgpPolEncoder(json.JSONEncoder):
         if isinstance(obj, pol_action_atom):
             return str(obj)
 
-        print "objs is", type(obj)
         return json.JSONEncoder.default(self, obj)
 
-policies = [
-"if (tags contain aaa and prefix_list is zyx and tags contain bbb) then (reject and setLP 100)",
-"if (tags contain aaa and prefix_list is zyx and tags contain bbb) then (reject and setLP 100) else (setLP 200)",
-"if (prefix_list is ccc) then (reject and setLP 100) else (reject and setMED 240 and setLP 210)",
-("if (prefix_list is ccc) then (reject and setLP 100) else "
-"(if (prefix_list is ccc) then (setLP 120) else (setMED 230))"),
-"if (prefix_list is ccc) then (setLP 120) else (setMED 230))",
-"reject and setLP 200",
-]
 
-
-for my_policy in policies:
-    print "Policy: ", my_policy
+def pol_to_json(my_policy):
     results = bgp_pol.parseString(my_policy)
     #print "results", results
     results = list(results) # list context
     data = json.dumps(results, cls=BgpPolEncoder, indent = 4)
-    print data
+    return data
+
 
 # also need to walk tree to build up a list of all tags present, and all prefix lists
 
