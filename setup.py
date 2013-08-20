39088b7287672837a5b792648f35df8635fa1096
sk2 <simon.knight@gmail.com>
2013-08-20 14:49:00 +0930

    add eigrp

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup (
      name = "autonetkit",
-     version = "0.5.9",
+     version = "0.5.11",
      description = 'Automatic configuration generation for emulated networks',
      long_description = 'Automatic configuration generation for emulated networks',
 
