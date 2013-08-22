5fffa2c007113f217fdd2f33f17f47b4403b9070
sk2 <simon.knight@gmail.com>
2013-08-22 18:56:08 +0930

    tidy

--- autonetkit/compiler.py
+++ autonetkit/compiler.py
@@ -9,7 +9,7 @@ def natural_sort(sequence, key=lambda s:s):
     import re
     sequence = list(sequence)
     def get_alphanum_key_func(key):
-        convert = lambda text: int(text) if text.isdigit() else text 
+        convert = lambda text: int(text) if text.isdigit() else text
         return lambda s: [convert(c) for c in re.split('([0-9]+)', key(s))]
     sort_key = get_alphanum_key_func(key)
     sequence.sort(key=sort_key)
