0865474e95a4b7f7c73fa5e4d2dbb5a60cdb0ac4
sk2 <simon.knight@gmail.com>
2013-08-26 18:19:07 +0930

    reverse mapping ips

--- autonetkit/collection/process.py
+++ autonetkit/collection/process.py
@@ -1,3 +1,5 @@
+import autonetkit.log as log
+
 def build_reverse_mappings_from_nidb(nidb):
     """Builds IP reverse mappings from NIDB"""
     rev_map = {
@@ -20,16 +22,34 @@ def build_reverse_mappings_from_anm_input(anm):
     """Builds reverse mappings from ANM input graph,
     assumes addresses have already been allocated onto input graph,
     either externally or by previous run"""
+
+    from collections import defaultdict
+    import netaddr
+
     g_in = anm['input']
     rev_map = {
             "loopbacks": {},
             "infra_interfaces": {},
+            "subnets": {},
     }
+
+    subnets = defaultdict(list)
 
     for node in g_in:
         rev_map["loopbacks"][str(node.loopback_v4)] = node
         for interface in node.physical_interfaces:
             rev_map["infra_interfaces"][str(interface.ipv4_address)] = interface
+
+            prefixlen = interface.ipv4_prefixlen
+            cidr_string = "%s/%s" % (interface.ipv4_address, prefixlen)
+            intermediate_subnet = netaddr.IPNetwork(cidr_string)
+            subnet_cidr_string = "%s/%s" % (intermediate_subnet.network, prefixlen)
+            subnet = netaddr.IPNetwork(subnet_cidr_string)
+            subnets[subnet].append(interface)
+
+    for subnet, interfaces in subnets.items():
+        subnet_str = str(subnet)
+        rev_map['subnets'][subnet_str] = "_".join(str(i.node) for i in interfaces)
 
     return rev_map
 
@@ -80,8 +100,6 @@ def reverse_map_routing(rev_map, data):
                 result.append((protocol, cd, iface.node))
     return result
 
-
-
 def reverse_map_path(rev_map, path, interfaces = False):
     """Returns list of nodes in path
     interfaces selects whether to return only nodes, or interfaces
@@ -101,3 +119,46 @@ def reverse_map_path(rev_map, path, interfaces = False):
             result.append(node)
 
     return result
+
+def substitute_ips(data, rev_map, interfaces = False):
+    import re
+    re_ip_address = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+    re_ip_loopback= r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/32"
+    re_ip_subnet = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}"
+    if not len(data):
+        log.info("No data provided to IP substitution, returning")
+        return data
+
+    def replace_ip(match):
+      match = match.group()
+      if match in rev_map['infra_interfaces']:
+        iface = rev_map['infra_interfaces'][match]
+        if interfaces:
+            named = "%s.%s" %(iface.id, iface.node)
+            return named
+        else:
+            return str(iface.node)
+      return match # no match, return the raw IP
+
+    def replace_loopbacks(match):
+      match = match.group()
+      # strip off the /32
+      loopback_ip = match[:-3]
+      if loopback_ip in rev_map['loopbacks']:
+        node = rev_map['loopbacks'][loopback_ip]
+        return str(node)
+      return match # no match, return the raw IP
+
+    def replace_subnet(match):
+      match = match.group()
+
+      if match in rev_map['subnets']:
+        subnet = rev_map['subnets'][match]
+        return str(subnet)
+      return match # no match, return the raw IP
+
+    # do loopbacks first
+    data = re.sub(re_ip_loopback, replace_loopbacks, data)
+    data = re.sub(re_ip_address, replace_ip, data)
+    # and for subnets ie ip/netmask
+    return re.sub(re_ip_subnet, replace_subnet, data)
