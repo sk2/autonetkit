1f8407e27896ad68e17975de95642565faa4938a
sk2 <simon.knight@gmail.com>
2013-10-17 15:04:04 +1030

    exclude multipoint edges from mpls te and rsvp

--- autonetkit/design/mpls.py
+++ autonetkit/design/mpls.py
@@ -4,6 +4,7 @@ import autonetkit.log as log
 def  mpls_te(anm):
     g_in = anm['input']
     g_phy = anm['phy']
+    g_l3_conn = anm['l3_conn']
     # add regardless, so allows quick check of node in anm['ospf'] in compilers
     g_mpls_te = anm.add_overlay("mpls_te")
     if not any(True for n in g_in if n.is_router and n.mpls_te_enabled):
@@ -12,5 +13,12 @@ def  mpls_te(anm):
 
     # te head end set if here
     g_mpls_te.add_nodes_from(g_in.routers())
+    # build up edge list sequentially, to provide meaningful messages for multipoint links
 
-    g_mpls_te.add_edges_from(g_phy.edges())
+    multipoint_edges = [e for e in g_l3_conn.edges()
+        if e.multipoint]
+    log.info("Excluding multi-point edges from MPLS TE topology: %s"
+        % ", ".join(str(e) for e in multipoint_edges))
+
+    edges_to_add = set(g_l3_conn.edges()) - set(multipoint_edges)
+    g_mpls_te.add_edges_from(edges_to_add)
