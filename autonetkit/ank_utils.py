e969d9a4dbb22cf35228cd9e0f4a3c7046a681de
Simon Knight <simon.knight@gmail.com>
2014-01-15 16:46:10 +1030

    new: dev: add merge script

--- autonetkit/ank_utils.py
+++ autonetkit/ank_utils.py
@@ -5,6 +5,7 @@ AutoNetkit Utilities
 #from anm import overlay_node, overlay_edge
 import autonetkit
 
+
 def unwrap_nodes(nodes):
     """Unwrap nodes"""
     try:
@@ -35,3 +36,27 @@ def wrap_nodes(overlay_graph, nodes):
 #TODO: remove duplicate of this in ank.py
     return ( autonetkit.anm.overlay_node(overlay_graph._anm, overlay_graph._overlay_id, node)
             for node in nodes)
+
+def merge_quagga_conf():
+    # helper function to merge the quagga config files into a single file for
+    # http://www.nongnu.org/quagga/docs/docs-multi/VTY-shell-integrated-configuration.html
+    import pkg_resources
+    import os
+    import time
+
+    templates_path =pkg_resources.resource_filename(__name__, "templates")
+    zebra_dir = os.path.join(templates_path, "quagga", "etc", "zebra")
+    #TODO: check the ordering - seems to matter
+    conf_files = ["zebra.conf.mako", "bgpd.conf.mako", "ospfd.conf.mako", "isisd.conf.mako"]
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    data = ["##Merged by AutoNetkit merge_quagga_conf on %s" % timestamp]
+    #TODO: replace newlines with !
+    for conf_file in conf_files:
+        filename = os.path.join(zebra_dir, conf_file)
+        with open(filename) as fh:
+            data.append(fh.read())
+
+    ospfd_conf = os.path.join(templates_path, "quagga.conf.mako")
+    with open(ospfd_conf, "w") as fh:
+        fh.write("\n".join(data))
+
