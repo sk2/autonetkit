d3aaba7cc706701c3a24cd95452d823e1161fdb3
Simon Knight <simon.knight@gmail.com>
2014-01-20 17:09:22 +1030

    Chg: Dev: remove render folder cache

--- autonetkit/render.py
+++ autonetkit/render.py
@@ -11,6 +11,8 @@ import pkg_resources
 from mako.exceptions import SyntaxException
 from mako.lookup import TemplateLookup
 
+#TODO: have same error handling block for each template render call
+
 #TODO: clean up cache enable/disable
 
 def resource_path(relative):
@@ -131,13 +133,10 @@ def render_inline(node, render_template_file, to_memory = True,
             return render_output
 
 #TODO: Add support for both src template and src folder (eg for quagga, servers)
-def render_node(node, folder_cache = None):
+def render_node(node):
     if not node.do_render:
         node.log.debug("Rendering disabled for node")
         return
-
-    if not folder_cache:
-        folder_cache = {}
 
     try:
         render_output_dir = node.render.dst_folder
@@ -213,23 +212,26 @@ def render_node(node, folder_cache = None):
     if render_base:
         #TODO: remove the folder cache for simplicity: doesn't sufficiently improve speed to warrant complexity
         #TODO: revert to shutil copy
-        if render_base in folder_cache:
-            src_folder = folder_cache[render_base]['folder']
-            fs_mako_templates = folder_cache[render_base]['templates']
+        if render_base:
+            render_base = resource_path(render_base)
+            fs_mako_templates = []
+            for root, dirnames, filenames in os.walk(render_base):
+                for filename in fnmatch.filter(filenames, '*.mako'):
+                    rel_root = os.path.relpath(root, render_base) # relative to fs root
+                    fs_mako_templates.append(os.path.join(rel_root, filename))
+
+            mako_tmp_dir = "cache"
+
 
             try:
                 shutil.rmtree(render_base_output_dir)
             except OSError:
                 pass # doesn't exist
-            shutil.copytree(src_folder, render_base_output_dir)
-
+            shutil.copytree(render_base, render_base_output_dir,
+                    ignore=shutil.ignore_patterns('*.mako'))
             for template_file in fs_mako_templates:
-                render_base_rel = resource_path(render_base)
-                template_file_path = os.path.normpath(os.path.join(render_base_rel,
-                    template_file))
+                template_file_path = os.path.normpath(os.path.join(render_base, template_file))
                 mytemplate = mako.template.Template(filename=template_file_path,
-# disabled cache
-#module_directory= mako_tmp_dir
                         )
                 dst_file = os.path.normpath((os.path.join(render_base_output_dir, template_file)))
                 dst_file, _ = os.path.splitext(dst_file) # remove .mako suffix
@@ -239,67 +241,17 @@ def render_node(node, folder_cache = None):
                         version_banner = version_banner,
                         date = date,
                         ))
-            return
-
-def cache_folders(nidb):
-    #TODO: see if this is required, revert to old renderer?
-    #TODO: look at copying to clobber folders rather than shutil remove
-    import tempfile
-    render_base = {node.render.base for node in nidb}
-    folder_cache_dir = tempfile.mkdtemp()
-    try:
-        render_base.remove(None)
-    except KeyError:
-        pass # Not present
-
-    folder_cache = {}
-    folder_cache['_folder_cache_dir'] = folder_cache_dir
-
-    for base in render_base:
-        folder_list = []
-        full_base = resource_path(base)
-        base_cache_dir = os.path.join(folder_cache_dir, base)
-        shutil.copytree(full_base, base_cache_dir,
-                ignore=shutil.ignore_patterns('*.mako'))
-
-        fs_mako_templates = []
-        for root, _, filenames in os.walk(full_base):
-            rel_root = os.path.relpath(root, full_base) # relative to fs root
-            folder_list.append(rel_root)
-            mako_templates = {f for f in filenames if f.endswith(".mako")}
-            for filename in mako_templates:
-                fs_mako_templates.append(os.path.join(rel_root, filename))
-
-#TODO: push templates into a cache dir
-
-        folder_cache[base] = {
-                'folder': base_cache_dir,
-                'templates': fs_mako_templates,
-                'folder_list': folder_list,
-                }
-
-    return folder_cache
-
+        return
 
 def render(nidb):
     #TODO: config option for single or multi threaded
     log.info("Rendering Network")
-    #folder_cache = cache_folders(nidb)
-    #render_single(nidb, folder_cache)
-    folder_cache = None
-    render_single(nidb, folder_cache)
+    render_single(nidb)
     render_topologies(nidb)
 
-#TODO: Also cache for topologies
-
-    if folder_cache:
-        folder_cache_dir = folder_cache['_folder_cache_dir']
-        shutil.rmtree(folder_cache_dir)
-
-
-def render_single(nidb, folder_cache = None):
+def render_single(nidb):
     for node in sorted(nidb):
-        render_node(node, folder_cache)
+        render_node(node)
 
 def render_topologies(nidb):
     for topology in nidb.topology:
