aef709778791015fc862da46f58e5c12622ba2b6
Simon Knight <simon.knight@gmail.com>
2014-01-24 13:22:14 +1100

    Bump version: 0.8.13 → 0.8.14

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup (
      name = "autonetkit",
-     version = "0.8.13",
+     version = "0.8.14",
      description = 'Automatic configuration generation for emulated networks',
      long_description = 'Automatic configuration generation for emulated networks',
 
