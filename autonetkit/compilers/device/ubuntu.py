90eab3ca81d496bf618e77f14835f7ff0d50f3e5
Simon Knight <simon.knight@gmail.com>
2014-03-27 15:59:56 +1030

    Chg: User: Initial support for multi-homed servers for VIRL-425

--- autonetkit/compilers/device/ubuntu.py
+++ autonetkit/compilers/device/ubuntu.py
@@ -34,86 +34,84 @@ class UbuntuCompiler(ServerCompiler):
             log.warning('Server %s is not directly connected to any routers'
                          % node)
             return
-        else:
-            gateway = gateway_list[0]  # choose first (and only gateway)
-            if len(gateway_list) > 1:
-                log.info('Server %s is multi-homed, using gateway %s'
-                         % (node, gateway))
+        elif len(gateway_list) > 1:
+                log.info('Server %s is multi-homed: using gateways %s'
+                         % (node, sorted(gateway_list)))
 
         # TODO: warn if server has no neighbors in same ASN (either in design or verification steps)
         # TODO: need to check that servers don't have any direct ebgp connections
 
-        gateway_edge_l3 = self.anm['layer3'].edge(node, gateway)
-        server_interface = gateway_edge_l3.src_int
-        server_interface_id = self.nidb.interface(server_interface).id
+        cloud_init_static_routes = []
 
-        gateway_interface = gateway_edge_l3.dst_int
+        for gateway in sorted(gateway_list):
+            gateway_edge_l3 = self.anm['layer3'].edge(node, gateway)
+            server_interface = gateway_edge_l3.src_int
+            server_interface_id = self.nidb.interface(server_interface).id
 
-        gateway_ipv4 = gateway_ipv6 = None
-        node.add_stanza("ip")
-        if node.ip.use_ipv4:
-            gateway_ipv4 = gateway_interface['ipv4'].ip_address
-        if node.ip.use_ipv6:
-            gateway_ipv6 = gateway_interface['ipv6'].ip_address
+            gateway_interface = gateway_edge_l3.dst_int
 
-        # TODO: look at aggregation
-        # TODO: catch case of ip addressing being disabled
+            gateway_ipv4 = gateway_ipv6 = None
+            node.add_stanza("ip")
+            if node.ip.use_ipv4:
+                gateway_ipv4 = gateway_interface['ipv4'].ip_address
+            if node.ip.use_ipv6:
+                gateway_ipv6 = gateway_interface['ipv6'].ip_address
 
-        # TODO: handle both ipv4 and ipv6
+            # TODO: look at aggregation
+            # TODO: catch case of ip addressing being disabled
 
-        # IGP advertised infrastructure pool from same AS
+            # TODO: handle both ipv4 and ipv6
 
-        static_routes_v4 = []
-        host_routes_v4 = []
-        for (asn, asn_routes)  in self.anm['ipv4'].data['infra_blocks'].items():
+            # IGP advertised infrastructure pool from same AS
+            static_routes_v4 = []
+            host_routes_v4 = []
+            for (asn, asn_routes)  in self.anm['ipv4'].data['infra_blocks'].items():
 
-           # host_routes_v4
-
-            for infra_route in asn_routes:
-                route_entry = {
-                'network': infra_route,
-                'prefix': infra_route.network,
-                'gw': gateway_ipv4,
-                'interface': server_interface_id,
-                'description': 'Route to infra subnet in AS %s via %s' \
-                % (asn, gateway),
-                }
-                route_entry = ConfigStanza(**route_entry)
-                if infra_route.prefixlen == 32:
-                    host_routes_v4.append(route_entry)
-                else:
-                    static_routes_v4.append(route_entry)
-
-        # eBGP advertised loopbacks in all (same + other) ASes
-
-        for (asn, asn_routes) in self.anm['ipv4'].data['loopback_blocks'
-                ].items():
-            for asn_route in asn_routes:
-                route_entry = {
-                    'network': asn_route,
-                    'prefix': asn_route.network,
+               # host_routes_v4
+               for infra_route in asn_routes:
+                    route_entry = {
+                    'network': infra_route,
+                    'prefix': infra_route.network,
                     'gw': gateway_ipv4,
                     'interface': server_interface_id,
-                    'description': 'Route to loopback subnet in AS %s via %s' \
-                        % (asn, gateway),
+                    'description': 'Route to infra subnet in AS %s via %s' \
+                    % (asn, gateway),
                     }
-                route_entry = ConfigStanza(**route_entry)
-                if asn_route.prefixlen == 32:
-                    host_routes_v4.append(route_entry)
-                else:
-                    static_routes_v4.append(route_entry)
+                    route_entry = ConfigStanza(**route_entry)
+                    if infra_route.prefixlen == 32:
+                        host_routes_v4.append(route_entry)
+                    else:
+                        static_routes_v4.append(route_entry)
 
-        # TODO: combine the above logic into single step rather than creating dict then formatting with it
+            # eBGP advertised loopbacks in all (same + other) ASes
 
-        cloud_init_static_routes = []
-        for entry in static_routes_v4:
-            formatted = 'route add -net %s gw %s dev %s' \
-                % (entry.network, entry.gw, entry.interface)
-            cloud_init_static_routes.append(formatted)
-        for entry in host_routes_v4:
-            formatted = 'route add -host %s gw %s dev %s' \
-                % (entry.prefix, entry.gw, entry.interface)
-            cloud_init_static_routes.append(formatted)
+            for (asn, asn_routes) in self.anm['ipv4'].data['loopback_blocks'
+                    ].items():
+                for asn_route in asn_routes:
+                    route_entry = {
+                        'network': asn_route,
+                        'prefix': asn_route.network,
+                        'gw': gateway_ipv4,
+                        'interface': server_interface_id,
+                        'description': 'Route to loopback subnet in AS %s via %s' \
+                            % (asn, gateway),
+                        }
+                    route_entry = ConfigStanza(**route_entry)
+                    if asn_route.prefixlen == 32:
+                        host_routes_v4.append(route_entry)
+                    else:
+                        static_routes_v4.append(route_entry)
+
+            # TODO: combine the above logic into single step rather than creating dict then formatting with it
+
+            for entry in static_routes_v4:
+                formatted = 'route add -net %s gw %s dev %s' \
+                    % (entry.network, entry.gw, entry.interface)
+                cloud_init_static_routes.append(formatted)
+            for entry in host_routes_v4:
+                formatted = 'route add -host %s gw %s dev %s' \
+                    % (entry.prefix, entry.gw, entry.interface)
+                cloud_init_static_routes.append(formatted)
 
         node.add_stanza("cloud_init")
         node.cloud_init.static_routes = cloud_init_static_routes
