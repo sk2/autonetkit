f1970a72775468e38a9aa12b44e814fb151e4e5a
Simon Knight <simon.knight@gmail.com>
2014-04-03 18:55:12 +1030

    chg: dev: prototype support for switches for gh-229

--- autonetkit/design/osi_layers.py
+++ autonetkit/design/osi_layers.py
@@ -156,11 +156,22 @@ def build_layer3(anm):
     g_l3 = anm.add_overlay("layer3")
     g_l3.add_nodes_from(g_l2 , retain=['label'])
     g_l3.add_nodes_from(g_in.switches(), retain=['asn'])
-    g_l3.add_edges_from(g_in.edges())
+    g_l3.add_edges_from(g_l2.edges())
 
     ank_utils.aggregate_nodes(g_l3, g_l3.switches())
     exploded_edges = ank_utils.explode_nodes(g_l3,
                                              g_l3.switches())
+
+    # also explode virtual switches
+    vswitches = [n for n in g_l3.nodes()
+    if n['layer2'].device_type == "switch"
+    and n['layer2'].device_subtype == "virtual"]
+
+    # explode each seperately?
+    vswitch_edges = []
+    for vswitch in vswitches:
+        vswitch_edges += ank_utils.explode_nodes(g_l3, [vswitch] )
+
     for edge in exploded_edges:
         edge.multipoint = True
         edge.src_int.multipoint = True
