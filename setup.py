5b9a922e102e146442b8d6a2a4df2cba702f3c63
Simon Knight <simon.knight@gmail.com>
2014-11-06 18:32:20 +1030

    Bump version: 0.10.10 → 0.10.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup (
      name = "autonetkit",
-     version = "0.10.10",
+     version = "0.10.11",
      description = 'Automatic configuration generation for emulated networks',
      long_description = 'Automatic configuration generation for emulated networks',
 
